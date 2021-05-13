4bfd1396e2c0a0786a0ed33e771ec0dbaf37cb3b
Sanskruti Gunde <82722578+SanskrutiGunde@users.noreply.github.com>
2021-05-13 12:35:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 const pic = "https://picsum.photos/400";
 ReactDOM.render(
   <div>
-    <h1 className="heading">My Favourite Foods</h1>
+    <h1 className="heading">Random images from picsum</h1>
     <img src={pic} />
   </div>,
   document.getElementById("root")
